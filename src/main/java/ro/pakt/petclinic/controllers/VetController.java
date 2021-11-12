f8416965f40b194c6a9676346ba228e0bb8f8748
Andrei Gheorghiu <gheorghiu.andrei@outlook.com>
2021-11-12 08:40:18 +0200

    add vet and owner controllers

---
+++ src/main/java/ro/pakt/petclinic/controllers/VetController.java
@@ -0,0 +1,12 @@
+package ro.pakt.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    public String listVets(){
+        return "vets/index";
+    }
+}
